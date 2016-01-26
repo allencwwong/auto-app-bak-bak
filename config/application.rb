c136685d443a79084ef79eb8b3580de60a796050
allencwwong <allencwwong@gmail.com>
2016-01-25 23:55:10 -0800

    reverse changes

--- config/application.rb
+++ config/application.rb
@@ -16,6 +16,7 @@ require "sprockets/railtie"
 Bundler.require(*Rails.groups)
 
 
+config.assets.initialize_on_precompile = false
 
 
 module AutoApp
@@ -33,7 +34,6 @@ module AutoApp
     # config.i18n.default_locale = :de
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
-config.assets.initialize_on_precompile = false
     config.active_record.raise_in_transactional_callbacks = true
   end
 end
